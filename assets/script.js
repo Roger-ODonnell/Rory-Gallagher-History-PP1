22562a6fb60d17d4ffc46be00f25f36ec6adb1e4
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2024-04-13 15:59:56 +0100

    add mobile/tablet navbar menu toggle

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
-function toggleMobileMenu(){
-    
+function OpenMenu() {
+  var navBarList = document.getElementById("mobile-sidebar");
+  var menuBar = document.getElementById("menu-bars");
+  var exitMenu = document.getElementById("menuExit");
+
+  menuBar.style.display = "none";
+  navBarList.style.display = "block";
+  exitMenu.style.display = "block";
 }
+
+function closeMenu() {
+    var navBarList = document.getElementById("mobile-sidebar");
+    var menuBar = document.getElementById("menu-bars");
+    var exitMenu = document.getElementById("menuExit");
+  
+    menuBar.style.display = "block";
+    navBarList.style.display = "none";
+    exitMenu.style.display = "none";
+  }
